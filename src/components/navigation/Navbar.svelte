27ec888cf1068f8e1f475ec3a591cfcb62c0c290
Sample Student <robstudent@altamontschool.org>
2020-05-27 12:50:56 +0000

    Got this shit working on the correct port!

---
+++ src/components/navigation/Navbar.svelte
@@ -0,0 +1,14 @@
+<style>
+  navbar {
+    display: grid;
+    grid-template-columns: 5fr 1fr 1fr 1fr;
+    grid-gap: 5px;
+  }
+</style>
+
+<navbar>
+  <a></a>
+  <a>Home</a>
+  <a>About</a>
+  <a>Blog</a>
+</navbar>
